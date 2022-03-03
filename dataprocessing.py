574f54b390f15aac3e8e41c86ba8140aa4378553
JediJoey <joeyzhou19951208@gmail.com>
2022-03-02 23:48:25 -0600

    initialize files and folders

---
+++ dataprocessing.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  2 22:48:35 2022
+
+@author: JoeyZhou
+"""
+
---
+++ experiments.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  2 23:12:37 2022
+
+@author: JoeyZhou
+"""
+
