de45bff22ef90b8e44a0109b7e65516bda71301f
JediJoey <joeyzhou19951208@gmail.com>
2022-03-23 21:32:01 -0500

    ten fold validation

--- experiment.jl
+++ experiment.jl
@@ -101,5 +101,5 @@ function experiments(name, num, initsize, k)
     end
 end
 
-performance = experiments("australian.dat", 650, 30, 1);
+performance = experiments("australian.dat", 600, 30, 10);
 println(performance);
